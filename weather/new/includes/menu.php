03d4aebc06981476e1acf00fe9ca90e5f712071c
Josh Dutcher <jdutcher@pksti.com>
2015-04-08 17:07:00 -0500

    still working and restructuring

---
+++ weather/new/includes/menu.php
@@ -0,0 +1,34 @@
+<?php
+
+$menu = array(
+	'Home' => 'index.php',
+	'Satellite' => 'satellite.php',
+	'Current Watches' => 'watches.php',
+	'Reflectivity' => 'reflectivity.php',
+	'Outages' => 'outages.php',
+	'Resources' => 'resources.php',
+	'Twitter' => 'twitter.php',
+	'About' => 'about.php'
+);
+
+?>
+
+<!-- Menu toggle -->
+<a href="#menu" id="menuLink" class="menu-link">
+    <!-- Hamburger icon -->
+    <span></span>
+</a>
+
+<div id="menu">
+    <div class="pure-menu">
+        <a class="pure-menu-heading" href="#">Company</a>
+
+        <ul class="pure-menu-list">
+        <?php
+        	foreach ($menu as $item => $file) {
+        		echo '<li class="pure-menu-item"><a href="' . $file . '" class="pure-menu-link">' . $item . '</a></li>';
+        	}
+        ?>
+        </ul>
+    </div>
+</div>
