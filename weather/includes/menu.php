c93af4e32d502f1cea23ddeadc3fd0e12f5967a9
Josh Dutcher <josh@sntmedia.com>
2016-12-08 12:13:01 -0600

    remove earthquakes page

--- weather/includes/menu.php
+++ weather/includes/menu.php
@@ -1,16 +1,16 @@
 <?php
 
 $menu = array(
-	'Radar' => array('url' => 'index.php', 'tooltip' => 'What you really came here for'),
-    'Satellite' => array('url' => 'satellite.php', 'tooltip' => ''),
-	'Base Reflectivity' => array('url' => 'reflectivity.php', 'tooltip' => ''),
-	'Watches/Warnings' => array('url' => 'watches.php', 'tooltip' => ''),
-	'Outlook' => array('url' => 'outlook.php', 'tooltip' => ''),
-	'Rainfall Amounts' => array('url' => 'rainfall.php', 'tooltip' => ''),
-	'Earthquakes' => array('url' => 'earthquakes.php', 'tooltip' => ''),
-	'Resources' => array('url' => 'resources.php', 'tooltip' => ''),
-	'Twitter' => array('url' => 'twitter.php', 'tooltip' => ''),
-	'About' => array('url' => 'about.php', 'tooltip' => '')
+    'Radar'             => array('url' => 'index.php', 'tooltip' => 'What you really came here for'),
+    'Satellite'         => array('url' => 'satellite.php', 'tooltip' => ''),
+    'Base Reflectivity' => array('url' => 'reflectivity.php', 'tooltip' => ''),
+    'Watches/Warnings'  => array('url' => 'watches.php', 'tooltip' => ''),
+    'Outlook'           => array('url' => 'outlook.php', 'tooltip' => ''),
+    'Rainfall Amounts'  => array('url' => 'rainfall.php', 'tooltip' => ''),
+    // 'Earthquakes'       => array('url' => 'earthquakes.php', 'tooltip' => ''),
+    'Resources'         => array('url' => 'resources.php', 'tooltip' => ''),
+    'Twitter'           => array('url' => 'twitter.php', 'tooltip' => ''),
+    'About'             => array('url' => 'about.php', 'tooltip' => ''),
 );
 
 ?>
@@ -25,19 +25,19 @@ $menu = array(
     <div class="pure-menu">
         <ul class="pure-menu-list">
         <?php
-        	foreach ($menu as $item => $file) {
-        		$class = 'pure-menu-item';
-        		if (basename($_SERVER['PHP_SELF']) == $file['url']) {
-        			$class .= ' selected';
-        		}
-        		echo '<li class="' . $class . '">';
-        		if (isset($file['new'])) {
-        			echo '<div class="new-menu-link"><img id="new" src="' . $imgpath . 'new.png"></div>';
-        		}
-        		echo '<a href="' . $file['url'] . '" class="pure-menu-link" title="' . $file['tooltip'] . '">' . $item . '</a>';
-        		echo '</li>';
-        	}
-        ?>
+foreach ($menu as $item => $file) {
+    $class = 'pure-menu-item';
+    if (basename($_SERVER['PHP_SELF']) == $file['url']) {
+        $class .= ' selected';
+    }
+    echo '<li class="' . $class . '">';
+    if (isset($file['new'])) {
+        echo '<div class="new-menu-link"><img id="new" src="' . $imgpath . 'new.png"></div>';
+    }
+    echo '<a href="' . $file['url'] . '" class="pure-menu-link" title="' . $file['tooltip'] . '">' . $item . '</a>';
+    echo '</li>';
+}
+?>
         </ul>
     </div>
 </div>
