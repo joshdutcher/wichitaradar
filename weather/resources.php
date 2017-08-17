5e6746d2334c0315e945578a305f1298d5f5d753
Josh Dutcher <josh@sntmedia.com>
2017-08-17 13:05:51 -0500

    add Become A Storm Spotter From Home to resources

--- weather/resources.php
+++ weather/resources.php
@@ -29,6 +29,7 @@
 			<ul>
 				<li><a href="http://outagemap.westarenergy.com/external/default.html" target="_blank">Westar Power Outages</a> Westar's map of current power outages</li>
 				<li><a href="http://wichway.org/wichway/CameraTours" target="_blank">KanDrive Camera Tours</a> Multiple webcam views of traffic and road conditions along a Wichita area route of your choice, on a single web page. Choose from US-54, I-235, I-35, K-96, and I-135</li>
+                <li><a href="http://www.improvenet.com/a/become-a-storm-spotter-from-home" target="_blank">Become A Storm Spotter From Home</a> Comprehensive list of resources on storm spotting, Skywarn, storm chasing, weather and climate terminology, and lots more.</li>
 			</ul>
 		</div>
 	</div>
