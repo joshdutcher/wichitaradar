e009c622d2fa0f4fa616342c71d47502be390151
Josh Dutcher <busychild424@gmail.com>
2018-05-03 15:30:14 -0500

    update twitter page

--- twitter.php
+++ twitter.php
@@ -21,18 +21,23 @@
 					<h2 class="content-subhead">Wichita Weather</h2>
 					<!-- my ICTWX list-->
 					<a class="twitter-timeline"
-						href="https://twitter.com/joshdutcher/ictwx"
-						data-widget-id="337783900774486016"
+						data-height="1200"
+						data-link-color="#87C2ED"
+						data-theme="dark"
+						href="https://twitter.com/joshdutcher/lists/ictwx1?ref_src=twsrc%5Etfw"
 						data-chrome="nofooter">
-						Wichita Weather
+						Wichita, KS area weather reports and storm chasers
 					</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 		        </div>
 
 		        <div class="pure-u pure-u-sm-1 pure-u-lg-1-2 pure-u-xl-1-3">
 					<h2 class="content-subhead">#ksstorms/#kswx/#ictwx hashtags</h2>
 					<!-- #ksstorms / #kswx / #ictwx -->
 					<a class="twitter-timeline"
+						data-height="1200"
+						data-link-color="#87C2ED"
+						data-theme="dark"
 						href="https://twitter.com/search?q=%23kswx+OR+%23ictwx+OR+%23ksstorms"
 						data-widget-id="337785602101637120"
 						data-chrome="nofooter">
@@ -45,12 +50,14 @@
 					<h2 class="content-subhead">US Weather</h2>
 					<!-- my uswx list -->
 					<a class="twitter-timeline"
-						href="https://twitter.com/joshdutcher/uswx"
-						data-widget-id="337784188226912256"
+						data-height="1200"
+						data-link-color="#87C2ED"
+						data-theme="dark"
+						href="https://twitter.com/joshdutcher/lists/uswx?ref_src=twsrc%5Etfw"
 						data-chrome="nofooter">
-						US Weather
+						Nationwide weather information and storm chasers
 					</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 		        </div>
 		    </div>
 		</div>
