6b121fc098412950697d17c294fd3518f29e990b
Josh Dutcher <josh.dutcher@adhocteam.us>
2019-09-18 14:57:34 -0500

    remove preparedness links from resources due to spammy link requests

--- resources.php
+++ resources.php
@@ -30,16 +30,6 @@
 				<li><a href="http://wichway.org/wichway/CameraTours" target="_blank">KanDrive Camera Tours</a> Multiple webcam views of traffic and road conditions along a Wichita area route of your choice, on a single web page. Choose from US-54, I-235, I-35, K-96, and I-135</li>
             </ul>
 
-            <h2 class="content-subhead">Severe Weather Preparedness Links</h2>
-            <ul>
-				<li><a href="http://www.aaastateofplay.com/staying-safe-outdoors-in-severe-weather/" target="_blank">Staying Safe Outdoors in Severe Weather</a> (Thanks to Mr. Cook and the kids at Meany Middle School for this link and the next!)</li>
-				<li><a href="https://www.austinrealestate.com/tornado-home-safety.php" target="_blank">Home Safety Preparedness for Tornadoes</a></li>
-				<li><a href="http://www.improvenet.com/a/become-a-storm-spotter-from-home" target="_blank">Become A Storm Spotter From Home</a> Comprehensive list of resources on storm spotting, Skywarn, storm chasing, weather and climate terminology, and lots more. (Thanks to Mia and the summer STEMjuniors meteorology group in Colorado Springs for this link!)</li>
-                <li><a href="http://expressfreightfinance.com/resources/avoiding-bad-weather-traveling/" target="_blank">Avoiding Bad Weather When Traveling</a> (Thanks to Mrs. Chapman and the kids at the Brenham Community Center in Texas for this link!)</li>
-                <li><a href="https://onlinemph.unr.edu/public-health-and-emergency-preparedness/" target="_blank">Public Health and Emergency Preparedness (via the University of Nevada, Reno)</a> (Thanks to Sarah Neale for this link and the next!)</li>
-                <li><a href="https://www.angieslist.com/articles/home-preparation-tips-weather-emergencies.htm" target="_blank">Home Preparation Tips for Weather Emergencies</a></li>
-			</ul>
-
 			<h2 class="content-subhead">Interesting Reading</h2>
 			<ul>
 				<li><a href="http://nowiknow.com/the-t-word-you-couldnt-talk-about/" target="_blank">The T-Word You Couldn’t Talk About</a> It used to be against NOAA's rules to mention the word tornado in a weather forecast.</li>
