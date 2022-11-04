5d7786f09d06b579d0636e516dad37a9112a52c5
Josh Dutcher <josh.dutcher@adhocteam.us>
2022-11-04 10:44:25 -0500

    add back in a wunderground radar

--- index.php
+++ index.php
@@ -63,9 +63,7 @@ https://github.com/emartinez-usgs/earthquake-widget
             <img class="pure-img-responsive" src="https://radar.weather.gov/ridge/standard/KICT_loop.gif" />
 
 			<!-- ****** Wunderground radar ***** -->
-            <!--
-            <img class="pure-img-responsive" src="http://radblast.wunderground.com/cgi-bin/radar/WUNIDS_map?station=ICT&brand=wui&num=6&delay=15&type=N0R&frame=0&scale=1.000&noclutter=0&showstorms=0&mapx=400&mapy=240&centerx=400&centery=240&transx=0&transy=0&showlabels=1&severe=0&rainsnow=0&lightning=0&smooth=0&rand=23867719&lat=0&lon=0&label=you">
-            -->
+            <img class="pure-img-responsive" src="https://s.w-x.co/staticmaps/wu/wu/wxtype1200_cur/ussln/animate.png">
         </div>
     </div>
 </div>
